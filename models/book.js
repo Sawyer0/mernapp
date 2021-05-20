b011c86cb70adcf527740e56560eb370384d8427
Dawan Sawyer <dawansawyer@gmail.com>
2021-05-19 20:51:18 -0400

    created routes

--- models/book.js
+++ models/book.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const gbookSchema = new Schema({
+const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
   date: { type: Date, default: Date.now }
 });
 
-const GBook = mongoose.model("Book", gbookSchema);
+const Book = mongoose.model("Book", bookSchema);
 
-module.exports = GBook;
+module.exports = Book;
